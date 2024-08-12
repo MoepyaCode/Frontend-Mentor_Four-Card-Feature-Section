2ca26dca5681a006f4ebf01b2b0109d3aad821df
MoepyaCode <kagisomoepya19@gmail.com>
2024-08-12 22:55:15 +0200

    🎉 Project init four-card-feature-section

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import bgPatternDesktop from './bg-pattern-desktop.svg';
+import heroDesktop from './hero-desktop.jpg';
+import heroMobile from './hero-mobile.jpg';
+import iconArrow from './icon-arrow.svg';
+import iconError from './icon-error.svg';
+
+export const Assets = {
+    bgPatternDesktop,
+    heroDesktop,
+    heroMobile,
+    iconArrow,
+    iconError,
+}
